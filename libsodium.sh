3c11d1771a5c44b19861ca3e030dfc273f33f49a
sunliang <sunliang711@163.com>
2018-11-18 13:56:04 +0800

    install libsodium.sh on debian and centos

---
+++ libsodium.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if (($EUID!=0));then
+    echo "Need run as root."
+    exit 1
+fi
+if command -v apt-get >/dev/null 2>&1;then
+    apt-get install build-essential -y
+    cd /tmp
+    wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
+    tar xf libsodium-1.0.16.tar.gz && cd libsodium-1.0.16
+    ./configure && make -j2 && make install
+    ldconfig
+elif command -v yum >/dev/null 2>&1;then
+    yum -y groupinstall "Development Tools"
+    cd /tmp
+    wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
+    tar xf libsodium-1.0.16.tar.gz && cd libsodium-1.0.16
+    ./configure && make -j2 && make install
+    echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
+    ldconfig
+fi
